1dd2e3f347e0d792efcf7c3a54b693bdf986eb43
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-07-23 07:29:24 -0300

    Some little progress in nodes connection...

--- src/index.2.ts
+++ src/index.2.ts
@@ -18,7 +18,7 @@ coin.addNewBlock({
   amount: 100,
 });
 
-const node = new Node(coin, PORT);
+const node = new Node(coin, 2829);
 
 process.argv.slice(2).forEach((otherPeerAddress) => {
   node.connect(otherPeerAddress).then((data) => console.log(data));
